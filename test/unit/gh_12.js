2de543b3643714c291979516f09ab8d0cd1a27bd
Andrew Sliwinski <andrewsliwinski@acm.org>
2014-09-15 22:25:18 -0700

    Add test coverage for GH-12

---
+++ test/unit/gh_12.js
@@ -0,0 +1,15 @@
+var test = require('tap').test;
+var sentiment = require('../../lib/index');
+
+var dataset = 'self-deluded';
+var result = sentiment(dataset);
+console.dir(result);
+
+test('synchronous positive', function (t) {
+    t.type(result, 'object');
+    t.equal(result.score, -2);
+    t.equal(result.comparative, -2);
+    t.equal(result.tokens.length, 1);
+    t.equal(result.words.length, 1);
+    t.end();
+});
