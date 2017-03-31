f6a1528df1f78a8d58ecac1ac144384f3d42eb56
Thor Berglie <thor.berglie@gmail.com>
2017-03-31 19:15:21 -0400

    More paring back of tokenizer

--- test/unit/tokenize.js
+++ test/unit/tokenize.js
@@ -5,7 +5,6 @@ test('spec', function (t) {
     t.type(tokenize, 'function');
     t.type(tokenize('foo'), 'object');
     t.equal(tokenize('foo bar').length, 2);
-    t.type(tokenize(), 'object');
 
     t.throws(function () {
         tokenize(123);
