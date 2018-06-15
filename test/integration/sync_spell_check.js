aaacd29f8619cca7097b3567e63e9a7026333596
Nicolas Santini <santini.nicolas@icloud.com>
2018-06-15 15:43:04 +1200

    making spell check optional and adding unit test

---
+++ test/integration/sync_spell_check.js
@@ -0,0 +1,26 @@
+var test = require('tap').test;
+var Sentiment = require('../../lib/index');
+var sentiment = new Sentiment();
+
+var input = 'I haite you';
+var result = sentiment.analyze(input, { spellCheck: true });
+
+test('synchronous negation', function (t) {
+    t.type(result, 'object');
+    t.equal(result.score, -3);
+    t.equal(result.comparative, -1);
+    t.equal(result.tokens.length, 3);
+    t.equal(result.words.length, 1);
+    t.end();
+});
+
+result = sentiment.analyze(input);
+
+test('synchronous negation', function (t) {
+    t.type(result, 'object');
+    t.equal(result.score, 0);
+    t.equal(result.comparative, 0);
+    t.equal(result.tokens.length, 3);
+    t.equal(result.words.length, 0);
+    t.end();
+});
