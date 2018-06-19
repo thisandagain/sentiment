8bb960e5a27e39241a1ab04e44dffd17e72e5654
Alexandre Silva <contato@silvalexandre.com>
2018-06-19 17:36:31 -0300

    refactor: Simplify nesting; Minor improvements

--- build/build.js
+++ build/build.js
@@ -6,69 +6,61 @@ const EMOJI_PATH = path.resolve(__dirname, 'Emoji_Sentiment_Data_v1.0.csv');
 const RESULT_PATH = path.resolve(__dirname, 'emoji.json');
 
 /**
- * Read emoji data from original format (CSV).
+ * Reads emoji data from original format (CSV).
  * @param  {object}   hash     Result hash.
  * @return {object}   hash     Result hash.
  */
-const processEmoji = async hash => {
+const processEmoji = async () => {
+    let hash = {};
     // Read file
-    try {
-        const data = await fs.readFileSync(EMOJI_PATH, 'utf8');
-        // Split data by new line
-        const lines = data.split(/\n/);
-        // Iterate over dataset and add to hash
-        for (const i in lines) {
-            const line = lines[i].split(',');
-            // Validate line
-            if (i === '0' || i === 0 || line.length !== 9) continue;
-            //  ^ Label     ^ Label    ^ Invalid
+    const data = await fs.readFileSync(EMOJI_PATH, 'utf8');
+    // Split data by new line
+    const lines = data.split(/\n/);
+    // Iterate over dataset and add to hash
+    for (const i in lines) {
+        const line = lines[i].split(',');
+        // Validate line
+        if (i === '0' || i === 0 || line.length !== 9) continue;
+        //  ^ Label     ^ Label    ^ Invalid
 
-            // Establish sentiment value
-            const emoji = String.fromCodePoint(line[1]);
-            const occurences = line[2];
-            const negCount = line[4];
-            const posCount = line[6];
-            const score = posCount / occurences - negCount / occurences;
-            const sentiment = Math.floor(5 * score);
+        // Establish sentiment value
+        const emoji = String.fromCodePoint(line[1]);
+        const occurences = line[2];
+        const negCount = line[4];
+        const posCount = line[6];
+        const score = posCount / occurences - negCount / occurences;
+        const sentiment = Math.floor(5 * score);
 
-            // Validate score
-            if (Number.isNaN(sentiment) || sentiment === 0) continue;
+        // Validate score
+        if (Number.isNaN(sentiment) || sentiment === 0) continue;
 
-            // Add to hash
-            hash[emoji] = sentiment;
-        }
-        return hash;
-    } catch (e) {
-        throw new Error(e);
+        // Add to hash
+        hash[emoji] = sentiment;
     }
+    return hash;
 };
 
 /**
- * Write sentiment score hash to disk.
+ * Writes sentiment score hash to disk.
  * @param  {object}   hash     Result hash
  * @return {object}   hash     Result hash
  */
-const finish = async hash => {
+const writeJSON = async hash => {
     const result = JSON.stringify(hash, null, 4);
-    try {
-        await fs.writeFileSync(RESULT_PATH, result);
-        return hash;
-    } catch (e) {
-        throw new Error(e);
-    }
+    await fs.writeFileSync(RESULT_PATH, result);
+    process.stderr.write(`Complete: ${Object.keys(hash).length} entries.\n`);
 };
 
-// Execute build process
+/**
+ * Executes build process.
+ * @return {undefined}
+ */
 const build = async () => {
     try {
-        let hash = {};
-        hash = await processEmoji(hash);
-        hash = await finish(hash);
-        process.stderr.write(
-            `Complete: ${Object.keys(hash).length} entries.\n`
-        );
+        const hash = await processEmoji();
+        writeJSON(hash);
     } catch (e) {
-        throw new Error(e);
+        console.log(e);
     }
 };
 build();
